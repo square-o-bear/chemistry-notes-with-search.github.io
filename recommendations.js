2a132615d01d1915df1991542f35497bdc3767b5
square-o-bear <mika.ushakov@gmail.com>
2026-02-03 23:16:08 +0300

    Перенос pages-nav
    + Перенос pages-nav в recommendations.js

--- recommendations.js
+++ recommendations.js
@@ -1,12 +1,14 @@
-const pagesNav = document.getElementsByClassName('pages-nav')[0];
+const pagesNav = document.getElementsByClassName('right-section')[0];
 
 pagesNav.innerHTML = `
-                <h3>Рекоминдуем</h3>
-                <ul class="pages-list">
-                    <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/index.html">Главная</a></li>
-                    <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/atom/atom.html">Атом</a></li>
-                    <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/molecules/molecules.html">Молекулы</a></li>
-                    <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/periodic/periodic.html">Периодическая система</a></li>
-                    <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/reactions/reactions.html">Химические реакции</a></li>
-                    <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/oxides/oxides.html">Оксиды</a></li>
-                </ul>`
+<div class="pages-nav">
+    <h3>Рекоминдуем</h3>
+    <ul class="pages-list">
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/index.html">Главная</a></li>
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/atom/atom.html">Атом</a></li>
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/molecules/molecules.html">Молекулы</a></li>
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/periodic/periodic.html">Периодическая система</a></li>
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/reactions/reactions.html">Химические реакции</a></li>
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/oxides/oxides.html">Оксиды</a></li>
+    </ul>
+</div>`
