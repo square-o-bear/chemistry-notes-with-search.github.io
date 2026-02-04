5d9b6eb2e6d086868b23455eff3e1250212fb6b5
square-o-bear <mika.ushakov@gmail.com>
2026-02-05 00:53:35 +0300

    navigation.js
    Перенос finder и recommendations в navigation.js

--- navigation.js
+++ navigation.js
@@ -1,6 +1,28 @@
+// Get the current page in notes
+let localpage = window.location.href.split('https://square-o-bear.github.io/chemistry-notes-with-search.github.io/').join('');
+
+if (localpage != window.location.href) {
+    console.log(localpage);
+    let lastVisited = JSON.parse(localStorage.getItem('last'));
+    if (localStorage.getItem('last') === null) {
+        lastVisited = [];
+    }
+    lastVisited = lastVisited.filter((page) => page !== localpage);
+    lastVisited.unshift(localpage);
+    lastVisited.slice(0, 11);
+    localStorage.setItem('last', JSON.stringify(lastVisited));
+}
+else {
+    console.warn('strange URL. Not started at "https://square-o-bear.github.io/chemistry-notes-with-search.github.io/"');
+}
+
+// Some tegs
+const pagesNav = document.getElementsByClassName('right-section')[0];
 const search_input = document.getElementById("search-input")
 const search_button = document.getElementById("search-button")
 const search_results = document.getElementById("search-results")
+
+// search keys for page
 const pages_to_search = {
     // INDEX
     "https://square-o-bear.github.io/chemistry-notes-with-search.github.io/index.html": [
@@ -315,6 +337,7 @@ const pages_to_search = {
     ]
 }
 
+// functions to find
 function comparison(key, input) {
     const minLength = Math.min(key.length, input.length);
     const maxLength = Math.max(key.length, input.length);
@@ -401,3 +424,36 @@ search_input.addEventListener("keydown", function(event) {
         finder();
     }
 });
+
+// theme changer / Рекоминдации / Последние просмотренные
+pagesNav.innerHTML =
+`<div class="pages-nav">
+    <button id='themeChange'>☀️&nbsp&nbsp&nbsp/&nbsp&nbsp&nbsp🌙</button>
+    <h3>Рекоминдуем</h3>
+    <ul class="pages-list">
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/index.html">Главная</a></li>
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/atom/atom.html">Атом</a></li>
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/molecules/molecules.html">Молекулы</a></li>
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/periodic/periodic.html">Периодическая система</a></li>
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/reactions/reactions.html">Химические реакции</a></li>
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/oxides/oxides.html">Оксиды</a></li>
+        <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/hydroxides/hydroxides.html">Гидроксиды</a></li>
+    </ul>
+</div>`;
+
+const themeChanger = document.getElementById('themeChange')
+if (localStorage.getItem('theme') === 'dark') {
+    document.documentElement.setAttribute('theme', 'dark');
+}
+const allTag = document.querySelectorAll('*');
+allTag.forEach((element) => {
+    element.style.transition = 'noone';
+});
+
+setTimeout(() => {allTag.forEach((element) => element.style.transition = 'all 0.3s ease');}, 0);
+
+themeChanger.addEventListener("click", (e) => {
+    let newTheme = (document.documentElement.getAttribute('theme') === 'dark' ? 'light' : 'dark');
+    document.documentElement.setAttribute('theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+})
