343132fe482f6a4461f172d81cbd8ae4036d4d43
square-o-bear <mika.ushakov@gmail.com>
2026-02-05 00:36:51 +0300

    Последние посещённые 0

--- recommendations.js
+++ recommendations.js
@@ -1,5 +1,26 @@
+// Get the current page in notes
+let localpage = window.location.href.split('https://square-o-bear.github.io/chemistry-notes-with-search.github.io/').join('');
+
+if (localpage != window.location.href) {
+    console.log(localpage);
+    let lastVisited = JSON.parse(localStorage.getItem('last'));
+    if (localStorage.getItem('last') === null) {
+        lastVisited = [];
+    }
+    lastVisited = lastVisited.filter((page) => page !== localpage);
+    lastVisited.unshift(localpage);
+    localpage.slice(0, 11);
+    console.log(lastVisited)
+    localStorage.setItem('last', JSON.stringify(lastVisited));
+}
+else {
+    console.warn('strange URL. Not started at "https://square-o-bear.github.io/chemistry-notes-with-search.github.io/"');
+}
+
 const pagesNav = document.getElementsByClassName('right-section')[0];
 
+
+// theme changer / Рекоминдации / Последние просмотренные
 pagesNav.innerHTML =
 `<div class="pages-nav">
     <button id='themeChange'>☀️&nbsp&nbsp&nbsp/&nbsp&nbsp&nbsp🌙</button>
@@ -13,27 +34,21 @@ pagesNav.innerHTML =
         <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/oxides/oxides.html">Оксиды</a></li>
         <li><a href="https://square-o-bear.github.io/chemistry-notes-with-search.github.io/hydroxides/hydroxides.html">Гидроксиды</a></li>
     </ul>
-</div>`
+</div>`;
 
 const themeChanger = document.getElementById('themeChange')
 if (localStorage.getItem('theme') === 'dark') {
     document.documentElement.setAttribute('theme', 'dark');
 }
 const allTag = document.querySelectorAll('*');
-console.log(allTag)
 allTag.forEach((element) => {
     element.style.transition = 'noone';
 });
 
-setTimeout(() => {
-    allTag.forEach((element) => {
-        element.style.transition = 'all 0.3s ease'
-        console.log(element)
-    });
-}, 0);
+setTimeout(() => {allTag.forEach((element) => element.style.transition = 'all 0.3s ease');}, 0);
 
 themeChanger.addEventListener("click", (e) => {
-    let newTheme = (document.documentElement.getAttribute('theme') === 'dark' ? 'light' : 'dark')
+    let newTheme = (document.documentElement.getAttribute('theme') === 'dark' ? 'light' : 'dark');
     document.documentElement.setAttribute('theme', newTheme);
     localStorage.setItem('theme', newTheme);
 })
